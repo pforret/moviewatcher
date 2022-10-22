db95af7bff513be32a5256c4370df9135630aed2
Peter Forret <peter@forret.com>
2022-10-23 00:05:24 +0200

    MOD: daily.yml run_job.sh

--- run_job.sh
+++ run_job.sh
@@ -3,8 +3,8 @@
 
 script_folder=$(dirname "$0")
 "$script_folder/moviewatcher" download https://datasets.imdbws.com/name.basics.tsv.gz
-"$script_folder/moviewatcher" download https://datasets.imdbws.com/title.akas.tsv.gz
 "$script_folder/moviewatcher" download https://datasets.imdbws.com/title.basics.tsv.gz
+#"$script_folder/moviewatcher" download https://datasets.imdbws.com/title.akas.tsv.gz
 #"$script_folder/moviewatcher" download https://datasets.imdbws.com/title.crew.tsv.gz
 #"$script_folder/moviewatcher" download https://datasets.imdbws.com/title.principals.tsv.gz
 #"$script_folder/moviewatcher" download https://datasets.imdbws.com/title.ratings.tsv.gz
