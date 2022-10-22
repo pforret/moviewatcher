1873a481a635c792cd8dfb57a2aaf4dc698076a2
Peter Forret <peter@forret.com>
2022-10-22 21:56:47 +0200

    MOD: bash_unit.yml run_job.sh

--- run_job.sh
+++ run_job.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
-./moviewatcher download https://datasets.imdbws.com/name.basics.tsv.gz
-./moviewatcher download https://datasets.imdbws.com/title.akas.tsv.gz
-./moviewatcher download https://datasets.imdbws.com/title.basics.tsv.gz
-./moviewatcher download https://datasets.imdbws.com/title.crew.tsv.gz
-./moviewatcher download https://datasets.imdbws.com/title.principals.tsv.gz
-./moviewatcher download https://datasets.imdbws.com/title.ratings.tsv.gz
 
-./moviewatcher split
+script_folder=$(dirname "$0")
+"$script_folder/moviewatcher" download https://datasets.imdbws.com/name.basics.tsv.gz
+"$script_folder/moviewatcher" download https://datasets.imdbws.com/title.akas.tsv.gz
+"$script_folder/moviewatcher" download https://datasets.imdbws.com/title.basics.tsv.gz
+"$script_folder/moviewatcher" download https://datasets.imdbws.com/title.crew.tsv.gz
+"$script_folder/moviewatcher" download https://datasets.imdbws.com/title.principals.tsv.gz
+"$script_folder/moviewatcher" download https://datasets.imdbws.com/title.ratings.tsv.gz
+
+"$script_folder/moviewatcher" split
